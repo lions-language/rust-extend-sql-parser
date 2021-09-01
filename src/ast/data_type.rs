cf04f82411475b574a7419dca18ec1d7c97ba683
lions-language <lions_language@163.com>
2021-09-01 23:24:22 +0800

    add format_type_with_optional_length

--- src/ast/data_type.rs
+++ src/ast/data_type.rs
@@ -1,6 +1,8 @@
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
 
+use std::fmt;
+
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub enum DataType {
     /// Fixed-length character type e.g. CHAR(10) / CHAR()
@@ -33,4 +35,23 @@ pub enum DataType {
     Bytea,
     // Custom(ObjectName)
     Array(Box<DataType>)
+}
+
+impl fmt::Display for DataType {
+    fn fmt(&self, f: fmt::Formatter) -> fmt::Result {
+        match self {
+            DataType::Char(size) => {
+            },
+            _ => {
+            }
+        }
+    }
+}
+
+fn format_type_with_optional_length(
+    f: &mut fmt::Formatter,
+    sql_type: &'static str,
+    len: &Option<u64>,
+) -> fmt::Result {
+    write!(f, "{}", sql_type)?;
 }
