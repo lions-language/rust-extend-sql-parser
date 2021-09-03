587d61e8d264302380caaed03f88385cf2e57331
lions-language <lions_language@163.com>
2021-09-03 21:21:52 +0800

    add statement

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -1,4 +1,16 @@
 mod data_type;
+mod ddl;
 
 use self::data_type::DataType;
+
+pub struct Ident {
+    pub value: String,
+    // 'xxx'
+    pub quote_style: Option<char>
+}
+
+pub enum Statement {
+    CreateTable {
+    }
+}
 
