65ea19ff13815362d3d86fc6cf8c060b9e32cb68
lions-language <lions_language@163.com>
2021-10-12 23:24:33 +0800

    update

---
+++ src/tokenizer.rs
@@ -0,0 +1,153 @@
+use std::iter::Peekable;
+use std::str::Chars;
+
+use std::fmt;
+
+use crate::dialect::keywords::{Keyword, ALL_KEYWORDS, ALL_KEYWORDS_INDEX};
+use crate::dialect::Dialect;
+
+pub enum Token {
+    EOF,
+    Word(Word),
+    /// An unsigned numeric literal
+    Number(String, bool),
+    /// A character that could not be tokenized
+    Char(char),
+    /// Single quoted string: i.e: 'string'
+    SingleQuotedString(String),
+    BackQuotedString(String),
+    /// "National" string literal: i.e: N'string'
+    NationalStringLiteral(String),
+    /// Hexadecimal string literal: i.e.: X'deadbeef'
+    HexStringLiteral(String),
+    /// Comma
+    Comma,
+    /// Whitespace (space, tab, etc)
+    Whitespace(Whitespace),
+    /// Double equals sign `==`
+    DoubleEq,
+    /// Equality operator `=`
+    Eq,
+    /// Not Equals operator `<>` (or `!=` in some dialects)
+    Neq,
+    /// Less Than operator `<`
+    Lt,
+    /// Greater Than operator `>`
+    Gt,
+    /// Less Than Or Equals operator `<=`
+    LtEq,
+    /// Greater Than Or Equals operator `>=`
+    GtEq,
+    /// Spaceship operator <=>
+    Spaceship,
+    /// Plus operator `+`
+    Plus,
+    /// Minus operator `-`
+    Minus,
+    /// Multiplication operator `*`
+    Mult,
+    /// Division operator `/`
+    Div,
+    /// Modulo Operator `%`
+    Mod,
+    /// String concatenation `||`
+    StringConcat,
+    /// Left parenthesis `(`
+    LParen,
+    /// Right parenthesis `)`
+    RParen,
+    /// Period (used for compound identifiers or projections into nested types)
+    Period,
+    /// Colon `:`
+    Colon,
+    /// DoubleColon `::` (used for casting in postgresql)
+    DoubleColon,
+    /// SemiColon `;` used as separator for COPY and payload
+    SemiColon,
+    /// Backslash `\` used in terminating the COPY payload with `\.`
+    Backslash,
+    /// Left bracket `[`
+    LBracket,
+    /// Right bracket `]`
+    RBracket,
+    /// Ampersand `&`
+    Ampersand,
+    /// Pipe `|`
+    Pipe,
+    /// Caret `^`
+    Caret,
+    /// Left brace `{`
+    LBrace,
+    /// Right brace `}`
+    RBrace,
+    /// Right Arrow `=>`
+    RArrow,
+    /// Sharp `#` used for PostgreSQL Bitwise XOR operator
+    Sharp,
+    /// Tilde `~` used for PostgreSQL Bitwise NOT operator
+    Tilde,
+    /// `<<`, a bitwise shift left operator in PostgreSQL
+    ShiftLeft,
+    /// `>>`, a bitwise shift right operator in PostgreSQL
+    ShiftRight,
+    /// Exclamation Mark `!` used for PostgreSQL factorial operator
+    ExclamationMark,
+    /// Double Exclamation Mark `!!` used for PostgreSQL prefix factorial operator
+    DoubleExclamationMark,
+    /// AtSign `@` used for PostgreSQL abs operator
+    AtSign,
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Token::EOF => f.write_str("EOF"),
+            Token::Word(ref w) => write!(f, "{}", w),
+            Token::Number(ref n, l) => write!(f, "{}{long}", n, long = if *l { "L" } else { "" }),
+            Token::Char(ref c) => write!(f, "{}", c),
+            Token::SingleQuotedString(ref s) => write!(f, "'{}'", s),
+            Token::BackQuotedString(ref s) => write!(f, "`{}`", s),
+            Token::NationalStringLiteral(ref s) => write!(f, "N'{}'", s),
+            Token::HexStringLiteral(ref s) => write!(f, "X'{}'", s),
+            Token::Comma => f.write_str(","),
+            Token::Whitespace(ws) => write!(f, "{}", ws),
+            Token::DoubleEq => f.write_str("=="),
+            Token::Spaceship => f.write_str("<=>"),
+            Token::Eq => f.write_str("="),
+            Token::Neq => f.write_str("<>"),
+            Token::Lt => f.write_str("<"),
+            Token::Gt => f.write_str(">"),
+            Token::LtEq => f.write_str("<="),
+            Token::GtEq => f.write_str(">="),
+            Token::Plus => f.write_str("+"),
+            Token::Minus => f.write_str("-"),
+            Token::Mult => f.write_str("*"),
+            Token::Div => f.write_str("/"),
+            Token::StringConcat => f.write_str("||"),
+            Token::Mod => f.write_str("%"),
+            Token::LParen => f.write_str("("),
+            Token::RParen => f.write_str(")"),
+            Token::Period => f.write_str("."),
+            Token::Colon => f.write_str(":"),
+            Token::DoubleColon => f.write_str("::"),
+            Token::SemiColon => f.write_str(";"),
+            Token::Backslash => f.write_str("\\"),
+            Token::LBracket => f.write_str("["),
+            Token::RBracket => f.write_str("]"),
+            Token::Ampersand => f.write_str("&"),
+            Token::Caret => f.write_str("^"),
+            Token::Pipe => f.write_str("|"),
+            Token::LBrace => f.write_str("{"),
+            Token::RBrace => f.write_str("}"),
+            Token::RArrow => f.write_str("=>"),
+            Token::Sharp => f.write_str("#"),
+            Token::ExclamationMark => f.write_str("!"),
+            Token::DoubleExclamationMark => f.write_str("!!"),
+            Token::Tilde => f.write_str("~"),
+            Token::AtSign => f.write_str("@"),
+            Token::ShiftLeft => f.write_str("<<"),
+            Token::ShiftRight => f.write_str(">>"),
+        }
+    }
+}
+
